ca5169ef61f4e96a585931ca6d3ca4a2b1544398
oscaralv <39628332+oscaralv@users.noreply.github.com>
2018-06-15 20:00:17 -0500

    Update TestMyController.java

--- vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ vertx-sample/src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -9,16 +9,16 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 public class TestMyController extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestMyController.class);
     
-//    @Test
+    @Test
     public void ok() throws Exception {
 //        String result = sendGet("https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-	String result = sendGet("http://192.168.10.129:5050/api/calculadora?operacion=suma&valor1=10&valor2=20");
+	String result = sendGet("http://192.168.10.129:5050/api/calculadoraoperacion=suma&valor1=10&valor2=20");
 	logger.info(result);
-	result = sendGet("http://192.168.10.129:5050/api/calculadora?operacion=resta&valor1=60&valor2=25");
+	result = sendGet("http://192.168.10.129:5050/api/calculadoraoperacion=resta&valor1=60&valor2=25");
 	logger.info(result);
-	result = sendGet("http://192.168.10.129:5050/api/calculadora?operacion=multiplica&valor1=50&valor2=30");
+	result = sendGet("http://192.168.10.129:5050/api/calculadoraoperacion=multiplica&valor1=50&valor2=30");
 	logger.info(result);
-	result = sendGet("http://192.168.10.129:5050/api/calculadora?operacion=divide&valor1=1000&valor2=100");
+	result = sendGet("http://192.168.10.129:5050/api/calculadoraoperacion=divide&valor1=1000&valor2=100");
 	logger.info(result);
     }
 
